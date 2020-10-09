1ef6c794d1eabbb18d7c6504379000995b165bfd
Liu-SD <lsd_9527@163.com>
2020-10-09 20:36:49 +0800

    accelerate image loading using weibo cdn

---
+++ js/description.js
@@ -0,0 +1,51 @@
+var desc = {
+    "https://tva1.sinaimg.cn/large/005UCIEPgy1gjjcrq0lp1j31400u0wia.jpg": "我们俩的第一张照片，太含蓄所以只拍了影子💑",
+    "https://tva3.sinaimg.cn/large/005UCIEPgy1gjjcsim92cj30u01hcq9u.jpg": "生日那天，婧婧写给刘斯盾的第一封情书",
+    "https://tvax3.sinaimg.cn/large/005UCIEPgy1gjjcttp25sj30u00u0gny.jpg": "婧婧送的生日蛋糕🎂",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcu3ndyzj31400u0whk.jpg": "生日晚餐，一起去吃小炒黄牛肉，还有晚餐后辣辣的💋",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcud3204j30u014078c.jpg": "岳麓山脚下，我们去吃绿绿的面面",
+    "https://tva1.sinaimg.cn/large/005UCIEPgy1gjjcurhn0nj30u0140765.jpg": "婧婧给买的云南白药，崴脚也不怕啦",
+    "https://tvax4.sinaimg.cn/large/005UCIEPgy1gjjcuximv4j30u01hctso.jpg": "银杏叶黄的季节，婧婧去图书馆接刘斯盾下班",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcv5gdtyj31400u0wk4.jpg": "婧婧烤的肉也太好吃了叭",
+    "https://tva3.sinaimg.cn/large/005UCIEPgy1gjjcvjf5zhj30u014076o.jpg": "那天队值，孙小姐给刘先生留的小纸条",
+    "https://tva3.sinaimg.cn/large/005UCIEPgy1gjjcvpg2v1j31400u07a7.jpg": "第一个学期结束啦，去吃云水谣~",
+    "https://tva1.sinaimg.cn/large/005UCIEPgy1gjjcvw7iocj31400u0wha.jpg": "婧婧钦点的向家人展示合照👫",
+    "https://tvax2.sinaimg.cn/large/005UCIEPgy1gjjcw2b45aj30u0140ad9.jpg": "假期分开的日子里，用照片分享彼此的生活",
+    "https://tva1.sinaimg.cn/large/005UCIEPgy1gjjcw7qclbj30u00hlq87.jpg": "2020💗爱你爱你",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcwj9qqfj31400u040o.jpg": "婧婧和毛茸茸怪兽，还是婧婧更可爱~",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcwovyvbj30u01qgtf4.jpg": "情人节，刘斯盾写给婧婧的情书💌",
+    "https://tvax4.sinaimg.cn/large/005UCIEPgy1gjjcww2j4wj30u00knjur.jpg": "情人节，婧婧写给刘斯盾的情书，一眼万年💌",
+    "https://tva4.sinaimg.cn/large/005UCIEPgy1gjjcx1rqs2j30m80go766.jpg": "超可爱的婧婧！那天婧婧喝了点🍷，更能聊了👀",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcx76aj0j318g0xch35.jpg": "那一天，终于见到你！",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcxcrh9bj32c0340hdx.jpg": "隔离期间，一顿费大厨外卖都能开心很久",
+    "https://tvax4.sinaimg.cn/large/005UCIEPgy1gjjcxk9f96j30m80e9wft.jpg": "终于出学校啦，吃豆人&条纹衫，还有乖巧的婧婧",
+    "https://tvax1.sinaimg.cn/large/005UCIEPgy1gjjcxv11xkj31400u0q45.jpg": "婧婧住院😭可把我心疼坏了",
+    "https://tva1.sinaimg.cn/large/005UCIEPgy1gjjcy0tefbj33402c04qt.jpg": "270!更多的是责任",
+    "https://tva1.sinaimg.cn/large/005UCIEPgy1gjjcyb66i9j30xc18ge81.jpg": "是彩虹的颜色💅🌈",
+    "https://tvax4.sinaimg.cn/large/005UCIEPgy1gjjcygiaguj318g0xce81.jpg": "我们在一起三百天啦",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcym4uajj32c0340u11.jpg": "那一天的婧婧可太紧张了😨",
+    "https://tvax4.sinaimg.cn/large/005UCIEPgy1gjjco5e3l3j31400u0mzj.jpg": "普通的一天，也是快乐的一天，有人说有夫妻相😁",
+    "https://tva4.sinaimg.cn/large/005UCIEPgy1gjjcysthctj33kg2ogkjo.jpg": "胡萝卜小金条，不知道尝起来是什么味道的",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcyz1egaj318g0xcb29.jpg": "今天是坐小火车的婧婧小朋友",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjcz4m389j30m80gomyi.jpg": "草帽男孩带着他的女孩去看电影🎥",
+    "https://tvax3.sinaimg.cn/large/005UCIEPgy1gjjczbqrqej32c0340qv8.jpg": "哇哦哦哦，七夕礼物是香水和可爱熊熊🐻",
+    "https://tvax1.sinaimg.cn/large/005UCIEPgy1gjjczhlkw1j33402c0qv9.jpg": "时间往复，爱你如初；情长纸短，吻你万千",
+    "https://tva3.sinaimg.cn/large/005UCIEPgy1gjjczhlkw1j33402c0qv9.jpg": "谁能想到我们俩还是画家cp呢🖌",
+    "https://tvax3.sinaimg.cn/large/005UCIEPgy1gjjczx10x9j32c0340u10.jpg": "今天去唱歌🎙",
+    "https://tvax4.sinaimg.cn/large/005UCIEPgy1gjjd02egpoj30u00u01ab.jpg": "甜心火龙果，婧婧就是小甜心呀",
+    "https://tva3.sinaimg.cn/large/005UCIEPgy1gjjd07jm3yj31400u0tnb.jpg": "亲吻我的公主 甜蜜像吃泡芙💕",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjd0c437mj31400u0kae.jpg": "海螺公主回到了海螺湾",
+    "https://tva2.sinaimg.cn/large/005UCIEPgy1gjjd0gzxljj31400u0x44.jpg": "cool girl & cool guy",
+    "https://tvax3.sinaimg.cn/large/005UCIEPgy1gjjd0n7yk6j31400u0ara.jpg": "二人剪影",
+}
+
+$(function () {
+    var imgpath = "https://liu-sd.gitee.io/lovestory/image";
+    // var imgpath = "image";
+    for (filename in desc) {
+        $("#GallaryUl").append(`<li data-contenttype="image" data-href="${filename}" , data-text="${desc[filename]}">
+    <img src="${filename}" />
+</li>`)
+    }
+    wall.refresh()
+});
